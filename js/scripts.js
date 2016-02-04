3e8e98373e0f3c955907ee6e2faa429ac941ad40
Otto Remse <otto.remse@gmail.com>
2016-02-04 17:04:21 +0100

    Added socket.io

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+
+
+var username ="";
+var room ="";
+var socket = io();
+socket.on('connect', function () {
+    socket.on('dicerollresponse', function(msg){
+    console.log(msg);
+    showAndLogDiceRoll(msg);
+    });
+  });
+var joinButton = document.getElementById("joinButton");
+joinButton.addEventListener("click",function(e){
+  username = document.getElementById("username").value;
+  room = document.getElementById("room").value;
+  socket.emit("join", {username: username, room: room});
+});
 var diceInput = document.getElementById("diceinput");
 diceInput.addEventListener("keydown", function(e){
     if(e.keyCode === 13){
@@ -12,12 +29,15 @@ r.onreadystatechange = function () {
 };
 function sendDiceRoll(){
   var rollText = diceInput.value;
+  socket.emit("dicerollrequest", {username:username,room:room,roll:rollText });
   //todo: validate rollText
-  r.open("get", "/roll/"+rollText, true);
-  r.send();
+  //r.open("get", "/roll/"+rollText, true);
+  //r.send();
 }
 function showAndLogDiceRoll(diceresult){
-  var resultDiv = document.getElementById("rollresult");
-  var resultText = "You rolled " + diceresult.dicesum + " (" + diceresult.dices.join() + "). With a modifier of " +diceresult.modifier + ". You got "+diceresult.obcount+" OB rolls";
-  resultDiv.innerHTML = resultText;
+  var resultText = diceresult.username+" rolled " + diceresult.dicesum + " (" + diceresult.dices.join() + "). With a modifier of " +diceresult.modifier + ". and got "+diceresult.obcount+" OB rolls";
+  var ul = document.getElementById("rollresult");
+   var li = document.createElement("li");
+   li.appendChild(document.createTextNode(resultText));
+   ul.appendChild(li);
 }
